6ed0e39e8507c7c13c873c296e0ea1ed95feb447
Ernesto-Nava-Hernandez <motorclub128@gmail.com>
2026-07-13 13:28:09 -0600

    Control basico de formulario con JS

---
+++ script.js
@@ -0,0 +1,12 @@
+const mainForm = () =>{
+    const email = document.querySelector('.email').value;
+    const text = document.querySelector('.text').value;
+
+    if(email.trim() === '' || text.trim() === ''){
+        alert('Los campos son obligatorios');
+    }else{
+        alert('Gracias por contactarnos')
+    }
+}
+
+document.getElementById('form-send').addEventListener('submit', mainForm);
